138bc4d78b6af3071827967c5089b7d36dd4cf0d
Daniela <Danielaptz@mail.ru>
2022-05-09 19:39:20 +0300

    Initial commit

---
+++ accelerometer/src/main/java/ru/mirea/ivanova/accelerometer/MainActivity.java
@@ -0,0 +1,62 @@
+package ru.mirea.ivanova.accelerometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener {
+
+    private TextView azimuthTextView;
+    private TextView pitchTextView;
+    private TextView rollTextView;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        azimuthTextView = (TextView) findViewById(R.id.azimuth_textview);
+        pitchTextView = (TextView) findViewById(R.id.pitch_textview);
+        rollTextView = (TextView) findViewById(R.id.roll_textview);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            azimuthTextView.setText(String.valueOf(sensorEvent.values[0]));
+            pitchTextView.setText(String.valueOf(sensorEvent.values[1]));
+            rollTextView.setText(String.valueOf(sensorEvent.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
